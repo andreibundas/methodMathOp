cfea01f578a85105c51335156393b949662f19bd
Andrei Bundas <andrei.bundas@gmail.com>
2020-07-08 10:55:51 +0300

    new project MethodMathOperations

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,13 +1,34 @@
 package org.fasttrackit;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+
+    public static void main(String[] args) {
+
+
+        // sum
+        Operations refOperation = new Operations();
+        int sum = refOperation.operation(5, 7);
+        System.out.println("The sum of nrOne and nrTwo is: " + sum);
+
+
+        // subtracting
+        Operations refOperation2 = new Operations();
+        int substraction = refOperation2.operation2(8, 4);
+        System.out.println("The substraction of nrOne from nrTwo is: " + substraction);
+
+
+        // multiplying
+        Operations refOperation3 = new Operations();
+        int multiply = refOperation3.operation3(3, 4);
+        System.out.println("The multuply of nrOne and nrTwo is: " + multiply);
+
+
+        // division
+        Operations refOperation4 = new Operations();
+        double division = refOperation4.operation4(17, 5);
+        System.out.println("The division of nrOne and nrTwo is: " + division);
+
     }
 }
+
---
+++ src/main/java/org/fasttrackit/Operations.java
@@ -0,0 +1,24 @@
+package org.fasttrackit;
+
+
+import static java.lang.System.*;
+
+public class Operations {
+
+    //method for sum
+    public int operation (int nrOne, int nrTwo){
+        return nrOne + nrTwo;
+    }
+    //method for subtraction
+    public int operation2 (int nrOne, int nrTwo){
+        return nrOne - nrTwo;
+    }
+    //method for multiplying
+    public int operation3 (int nrOne, int nrTwo){
+        return nrOne * nrTwo;
+    }
+    //method for division
+    public int operation4 (int nrOne, int nrTwo) {
+        return nrOne / nrTwo;
+    }
+}
