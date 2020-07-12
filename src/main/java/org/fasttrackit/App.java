2b4a39c90a97ccb18e2922f0178e65a3330d0dbb
Andrei Bundas <andrei.bundas@gmail.com>
2020-07-12 21:25:01 +0300

    Exercise Overloading

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -5,29 +5,44 @@ public class App {
 
     public static void main(String[] args) {
 
-
         // sum
         Operations refOperation = new Operations();
         int sum = refOperation.operation(5, 7);
         System.out.println("The sum of nrOne and nrTwo is: " + sum);
 
+        // sum - overloading
+        Operations refOperation1 = new Operations();
+        double sum2 = refOperation1.operation(5.5,2.3);
+        System.out.println(" Overloading: The sum of nrOne and nrTwo is: " + sum2);
 
         // subtracting
         Operations refOperation2 = new Operations();
         int substraction = refOperation2.operation2(8, 4);
         System.out.println("The substraction of nrOne from nrTwo is: " + substraction);
 
+        // subtracting - overloading
+        Operations refOperation22 = new Operations();
+        double substraction2 = refOperation22.operation2(6.7, 5.4);
+        System.out.println("Overloading: The substraction of nrOne from nrTwo is: " + substraction2);
 
         // multiplying
         Operations refOperation3 = new Operations();
         int multiply = refOperation3.operation3(3, 4);
         System.out.println("The multuply of nrOne and nrTwo is: " + multiply);
-
+        // multiplying - overloading
+        Operations refOperation33 = new Operations();
+        double multiply2 = refOperation33.operation3(4.3, 2.9);
+        System.out.println("Overloading: The multuply of nrOne and nrTwo is: " + multiply2);
 
         // division
         Operations refOperation4 = new Operations();
-        double division = refOperation4.operation4(17, 5);
+        int division = refOperation4.operation4(4,2);
         System.out.println("The division of nrOne and nrTwo is: " + division);
+
+        // division - overloading
+        Operations refOperation44 = new Operations();
+        double division2 = refOperation44.operation4(5.4,2.2);
+        System.out.println("Overloading: the division of nrOne and nrTwo is: " + division2);
 
     }
 }
--- src/main/java/org/fasttrackit/Operations.java
+++ src/main/java/org/fasttrackit/Operations.java
@@ -1,24 +1,44 @@
 package org.fasttrackit;
 
 
-import static java.lang.System.*;
-
 public class Operations {
 
     //method for sum
     public int operation (int nrOne, int nrTwo){
         return nrOne + nrTwo;
     }
+    // overloading - method for sum
+    public double operation (double nrOne, double nrTwo){
+        return nrOne + nrTwo;
+    }
+
+
     //method for subtraction
     public int operation2 (int nrOne, int nrTwo){
         return nrOne - nrTwo;
     }
+    // overloading -  method for subtraction
+    public double operation2 (double nrOne, double nrTwo){
+        return nrOne - nrTwo;
+    }
+
     //method for multiplying
     public int operation3 (int nrOne, int nrTwo){
         return nrOne * nrTwo;
     }
-    //method for division
-    public int operation4 (int nrOne, int nrTwo) {
+    // overloading - method for multiplying
+    public double operation3 (double nrOne, double nrTwo) {
+        return nrOne * nrTwo;
+    }
+
+        //method for division
+        public int operation4 (int nrOne, int nrTwo) {
+            return nrOne / nrTwo;
+        }
+
+    // overloading - method for division
+    public double operation4 (double nrOne, double nrTwo) {
         return nrOne / nrTwo;
     }
+
 }
